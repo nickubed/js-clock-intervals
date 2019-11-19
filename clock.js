dad2b8500611d07e462016083c2366e2ccb518bc
nickubed <nhpquandt@gmail.com>
2019-11-19 09:03:11 -0800

    Updated with current time.

--- clock.js
+++ clock.js
@@ -6,9 +6,9 @@ let seconds = document.getElementById('second');
 let minutes = document.getElementById('minute');
 let hours = document.getElementById('hour');
 
-let rotation = 6;
-let minuteChange = 6;
-let hourChange = 30;
+let rotation = date.getSeconds() * 6;
+let minuteChange = date.getMinutes() * 6;
+let hourChange = date.getHours() * 30;
 let secondsCount = 0;
 let minsCount = 0;
 
@@ -16,27 +16,27 @@ let minsCount = 0;
 const secondRotation = () =>{
     seconds.style.transform = "rotate(" + rotation + "deg)";
     rotation += 6;
-    secondsCount ++
+    // secondsCount ++
     minRotation();
 }
 
 const minRotation = () =>{
-    if(secondsCount % 60 == 0){
+    // if(secondsCount % 60 == 0){
         minutes.style.transform = "rotate(" + minuteChange + "deg)";
-        minuteChange += 6;
-        minsCount ++
-        secondsCount = 0;
+        minuteChange += .1;
+        // minsCount ++
+        // secondsCount = 0;
         hourRotation();
-    }
+    // }
 
 }
 
 const hourRotation = () =>{
-    if(minsCount % 60 == 0){
+    // if(minsCount % 60 == 0){
         hours.style.transform = "rotate(" + hourChange + "deg)";
-        hourChange += 30;
-        minsCount == 0;
-    }
+        hourChange += .003;
+        // minsCount == 0;
+    // }
 }
 
 setInterval(secondRotation, 1000);
